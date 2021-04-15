b2d0953fcc5943850b8ac30936965ac03fb1e20a
valedipaola <80895508+valedipaola@users.noreply.github.com>
2021-04-15 09:36:27 -0300

    Add files via upload

---
+++ EJDOWHILE.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejdowhile;
+
+/**
+ *
+ * @author pc05
+ */
+public class EJDOWHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int i=0;
+        System.out.println("Números del 1 al 100");
+        do {System.out.println(i);
+        i++;
+                }
+        while (i<100);
+    }
+        }
+    
+    
+

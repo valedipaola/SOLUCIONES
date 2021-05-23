19aa1d02cacebe88a16c7929adf09e42acb5136f
valedipaola <80895508+valedipaola@users.noreply.github.com>
2021-05-22 23:19:51 -0300

    Add files via upload

---
+++ EJMAYO1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejmayo1;
+
+import  java.util.Scanner ;
+/**
+ *
+ * @author Valentna
+ */
+public class EJMAYO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    // Lógica de aplicación de código TODO aquí
+        Días de escáner =  nuevo  escáner ( System . In);
+        Sistema . fuera . println ( " Ingrese un número para saber el día que es: " );
+        int dia = dias . nextInt ();
+        String nomdia;
+        switch (dia) {
+            caso  1 : nomdia =  " Lunes " ;
+                romper ;
+            caso  2 : nomdia =  " Martes " ;
+                romper ;
+            caso  3 : nomdia =  " Miercoles " ;
+                romper ;
+            caso  4 : nomdia =  " Jueves " ;
+                romper ;
+               caso  5 : nomdia =  " Viernes " ;
+                romper ;
+            caso  6 : nomdia =  " Sábado " ;
+                romper ;
+            caso  7 : nomdia =  " Domingo " ;
+                romper ;
+            predeterminado : nomdia = " Día inválido " ;
+                romper ;     
+        }
+        Sistema . fuera . println (nomdia);
+        
+    }
+    
+}
+    }
+    
+}
---
+++ EJMAYO2.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejmayo2;
+
+import  java.util.Scanner ;
+/**
+ *
+ * @author Valentna
+ */
+public class EJMAYO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner meses =  new  Scanner ( System . In);
+        Sistema . fuera . println ( " Ingrese un número para saber el mes que es: " );
+        int mes = meses . nextInt ();
+        Nombres de cadena ;
+        cambiar (mes) {
+            caso  1 : nommes =  " Enero " ;
+                romper ;
+            caso  2 : nommes =  " Febrero " ;
+                romper ;
+            caso  3 : nommes =  " Marzo " ;
+                romper ;
+            caso  4 : nommes =  " Abril " ;
+                romper ;
+               caso  5 : nommes =  " Mayo " ;
+                romper ;
+            caso  6 : nommes =  " Junio " ;
+                romper ;
+            caso  7 : nommes =  " Julio " ;
+                romper ;
+            caso  8 : nommes =  " Agosto " ;
+                romper ;
+            caso  9 : nommes =  " Septiembre " ;
+                romper ;
+            caso  10 : nommes =  " Octubre " ;
+                romper ;
+            caso  11 : nommes =  " Noviembre " ;
+                romper ;
+            caso  12 : nommes =  " Diciembre " ;
+                romper ;
+            predeterminado : nommes = " mes inválido " ;
+                romper ;     
+        }
+        Sistema . fuera . println (nommes);
+        
+    }
+    
+}
+    }
+    
+}
